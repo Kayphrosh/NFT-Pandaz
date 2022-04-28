a526101f4081c80b556a755ecc1966b6c56759f5
Kayphrosh <olakunbiolabode@yahoo.com>
2022-04-28 10:50:14 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -3,24 +3,23 @@ const slides = document.querySelectorAll('.slide');
 const leftBtn = document.getElementById('left');
 const rightBtn = document.getElementById('right');
 const progress = document.getElementById('progress');
-const circles = document.querySelectorAll('.circle');
-
+const circles = document.querySelectorAll('.navbar a');
+// const link = document.querySelectorAll('.navbar a');
 
 let activeSlide = 0
 
 rightBtn.addEventListener('click', () => {
     activeSlide++
     if(activeSlide >  slides.length - 1) {
-        activeSlide = 0
+        activeSlide = 0;
     }
     setBgToBody()
     setActiveSlide()
 })
 leftBtn.addEventListener('click', () => {
-    // currentActive--
     activeSlide--
     if(activeSlide < 0) {
-        activeSlide = slides.length - 1
+        activeSlide = slides.length - 1;
     }
     setBgToBody()
     setActiveSlide()
@@ -36,33 +35,38 @@ function setActiveSlide(){
     slides.forEach((slide) => slide.classList.remove('active'))
     slides[activeSlide].classList.add('active')
 }
-let currentActive = 1 
+
+let currentActive = 0
 
 rightBtn.addEventListener('click', () => {
     currentActive++
-    if(currentActive > circles.length) {
-        currentActive = 1
-    }
+    if(currentActive > circles.length - 1) {
+        currentActive = 0
+    } 
     update()
 })
 leftBtn.addEventListener('click', () => {
     currentActive--
-    if(currentActive < 1) {
-        currentActive = slides.length
+    if(currentActive < 0) {
+        currentActive = slides.length - 1
     }
     update()
 })
 function update() {
         circles.forEach((circle, idx) => {
-        if(idx < currentActive) {
-            circle.classList.add('active-step')
-        } else {
-            circle.classList.remove('active-step')
+        if(idx === currentActive) {
+            circle.classList.add('active-link')
+            circle.style.color = '#a67c00';
+            circle.style.borderBottom = '1px solid #a67c00';
+        } else{
+            circle.classList.remove('active-link')
+            circle.style.color = '#ffffff';
+            circle.style.border = 'none';
         }
-    })
-        const actives = document.querySelectorAll('.active-step')
 
-        progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
+    })
+        // const actives = document.querySelectorAll('.active-link')
+        // circles.style.color = (actives.length - 1) / (circles.length - 1) * 100 + '#fff'
 }
 
 let toggleNavstatus = false;
